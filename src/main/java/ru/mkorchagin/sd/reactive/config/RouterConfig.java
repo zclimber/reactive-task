094498929d762a5ab88325097df8f67aeeba690b
ME <me@example.com>
2019-03-23 18:57:37 +0300

    Config that works w Reactive Thymeleaf

---
+++ src/main/java/ru/mkorchagin/sd/reactive/config/RouterConfig.java
@@ -0,0 +1,48 @@
+package ru.mkorchagin.sd.reactive.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import ru.mkorchagin.sd.reactive.web.IndexHandler;
+import ru.mkorchagin.sd.reactive.web.ItemsHandler;
+import ru.mkorchagin.sd.reactive.web.UserHandler;
+
+@Configuration
+public class RouterConfig {
+    private final ItemsHandler itemsHandler;
+    private final UserHandler usersHandler;
+    private final IndexHandler indexHandler;
+
+    public RouterConfig(ItemsHandler itemsHandler, UserHandler usersHandler, IndexHandler indexHandler) {
+        this.itemsHandler = itemsHandler;
+        this.usersHandler = usersHandler;
+        this.indexHandler = indexHandler;
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> itemsHandlerRouter() {
+        return RouterFunctions.route()
+                .GET("/listItems", itemsHandler::listAllItemsView)
+                .GET("/addItem", itemsHandler::addItemView)
+                .POST("/addItem", itemsHandler::addItem)
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> usersHandlerRouter() {
+        return RouterFunctions.route()
+                .GET("/addUser", usersHandler::addUserView)
+                .POST("/addUser", usersHandler::addUser)
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> indexHandlerRouter() {
+        return RouterFunctions.route()
+                .GET("/", indexHandler::indexView)
+                .POST("/addUser", indexHandler::indexView)
+                .build();
+    }
+}
--- src/main/java/ru/mkorchagin/sd/reactive/config/WebConfig.java
+++ src/main/java/ru/mkorchagin/sd/reactive/config/WebConfig.java
@@ -1,35 +1,69 @@
 package ru.mkorchagin.sd.reactive.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
-import ru.mkorchagin.sd.reactive.web.ItemsHandler;
-import ru.mkorchagin.sd.reactive.web.UserHandler;
+import org.springframework.web.reactive.config.ViewResolverRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.thymeleaf.spring5.ISpringWebFluxTemplateEngine;
+import org.thymeleaf.spring5.SpringWebFluxTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.reactive.ThymeleafReactiveViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
+@Configuration
 @EnableWebFlux
-public class WebConfig {
-    @Autowired
-    ItemsHandler itemsHandler;
-    @Autowired
-    UserHandler usersHandler;
+@EnableConfigurationProperties(ThymeleafProperties.class)
+public class WebConfig implements ApplicationContextAware, WebFluxConfigurer {
+    private ApplicationContext context;
 
-    @Bean
-    public RouterFunction<ServerResponse> itemsHandlerRouter() {
-        return RouterFunctions.route()
-                .GET("/list", itemsHandler::listAllItemsView)
-                .GET("/addItem", itemsHandler::addItemView)
-                .POST("/addItem", itemsHandler::addItem)
-                .build();
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
     }
 
     @Bean
-    public RouterFunction<ServerResponse> usersHandlerRouter() {
-        return RouterFunctions.route()
-                .GET("/addUser", usersHandler::addUserView)
-                .POST("/addUser", usersHandler::addUser)
-                .build();
+    public SpringResourceTemplateResolver thymeleafTemplateResolver() {
+
+        final SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(this.context);
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCacheable(false);
+        resolver.setCheckExistence(false);
+        return resolver;
+
+    }
+
+    @Bean
+    public ISpringWebFluxTemplateEngine thymeleafTemplateEngine() {
+        // We override here the SpringTemplateEngine instance that would otherwise be
+        // instantiated by
+        // Spring Boot because we want to apply the SpringWebFlux-specific context
+        // factory, link builder...
+        final SpringWebFluxTemplateEngine templateEngine = new SpringWebFluxTemplateEngine();
+        templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafReactiveViewResolver thymeleafChunkedAndDataDrivenViewResolver() {
+        final ThymeleafReactiveViewResolver viewResolver = new ThymeleafReactiveViewResolver();
+        viewResolver.setTemplateEngine(thymeleafTemplateEngine());
+//        viewResolver.setOrder(1);
+//        viewResolver.setViewNames(new String[]{"home"});
+        viewResolver.setResponseMaxChunkSizeBytes(8192); // OUTPUT BUFFER size limit
+        return viewResolver;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(thymeleafChunkedAndDataDrivenViewResolver());
     }
 }
